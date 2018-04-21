f7a0be8072783c547a43d30d71f72a27701e1fb7
Marcin Wojcik <marcincyniu@gmail.com>
2018-04-21 09:03:01 -0700

    feature/79 Add promary key to DIscount Model

--- app/models/Discount.php
+++ app/models/Discount.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Discount extends Model {
 
     protected $table = 'discounts';
-
+    protected $primaryKey = 'code';
     protected $fillable = [];
 }
