fca472468c2719cd9fe07a9b2993d18547a04cef
Marcin Wojcik <marcincyniu@gmail.com>
2018-05-15 19:59:40 -0700

    Misc comment doubled class CreateUsersTable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,32 +4,34 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+// Commented because conflict with file: /home/developer/code/daniel_tyack/gatku_55/database/migrations/2015_03_27_180608_create_users_table.php
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
-}
+//class CreateUsersTable extends Migration
+//{
+//    /**
+//     * Run the migrations.
+//     *
+//     * @return void
+//     */
+//    public function up()
+//    {
+//        Schema::create('users', function (Blueprint $table) {
+//            $table->increments('id');
+//            $table->string('name');
+//            $table->string('email')->unique();
+//            $table->string('password');
+//            $table->rememberToken();
+//            $table->timestamps();
+//        });
+//    }
+//
+//    /**
+//     * Reverse the migrations.
+//     *
+//     * @return void
+//     */
+//    public function down()
+//    {
+//        Schema::dropIfExists('users');
+//    }
+//}
