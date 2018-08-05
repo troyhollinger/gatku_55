66237c2d090c7b989e8b002f54c487b0b72f6962
Marcin Wojcik <marcincyniu@gmail.com>
2018-08-05 12:13:41 -0700

    feature/126 Clean database for Crist page. Don't run this query on GATKU.COM.

---
+++ database/patches/apply only on crist/2018-08-05_clean_database_only_on_crist.sql
@@ -0,0 +1,37 @@
+
+-- Remove all orders and related records
+DELETE FROM order_item_addons;
+DELETE FROM order_items;
+DELETE FROM shipping_requests;
+DELETE FROM shipping_tracks;
+DELETE FROM orders;
+
+-- Remove all customers
+DELETE FROM customers;
+
+--Remove all you_images
+DELETE FROM you_images;
+
+-- Remove all not used product on page.
+DELETE FROM products
+WHERE name IN (
+    'Dinner Getter',
+    'Blue Water',
+    'Lionfish',
+    'Inshore Shrinker',
+    'Offshore Striker',
+    'G-String',
+    'Cable Kit',
+    'Black Knife',
+    'Extra Tip',
+    'Cable',
+    'BigGame',
+    'FinFlashers',
+    "9'ER",
+    'SuperHero',
+    'Comfort',
+    'Signature Glass',
+    'Barbed Glass',
+    'Sliptip Glass',
+    'Flopper Glass'
+);
