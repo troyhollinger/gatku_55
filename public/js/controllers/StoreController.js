bc921c60bcd7955984bdb38b18a61beb29eb0f2c
Marcin Wojcik <marcincyniu@gmail.com>
2018-08-08 21:22:49 -0700

    feature/130 Shelf managed in admin and displayed on front.

--- public/js/controllers/StoreController.js
+++ public/js/controllers/StoreController.js
@@ -3,38 +3,55 @@
 
     function StoreController($scope, Product, Shelves, $exceptionHandler) {
 
-        $scope.heads = [];
+        $scope.shortNamesForProductTypes = [
+            'Head',
+            'Glass',
+            'Pole'
+        ];
 
-        $scope.poles = [];
+        $scope.displayLengthForProduct = [
+            'Glass'
+        ];
 
-        $scope.shrinker = [];
-
-        $scope.extras = [];
-
-        $scope.apparel = [];
+        $scope.displayShortNameExtensionForProduct = [
+            'Pole'
+        ];
 
         $scope.init = function () {
             $scope.getStore();
             $scope.getShelves();
         };
 
+        $scope.getThumbClass = function(product){
+            return 'product-thumb-container-' + product.type.name;
+        };
+
+        $scope.getLinkClass = function(product){
+            return 'product-link-container-' + product.type.name;
+        };
+
+        $scope.shelfProductNameToDisplay = function(product) {
+            if ($scope.shortNamesForProductTypes.indexOf(product.type.name) !== -1) {
+                return product.shortName;
+            }
+            return product.name;
+        };
+
+        $scope.shelfProductDisplayLength = function(product) {
+            if ($scope.displayLengthForProduct.indexOf(product.type.name) !== -1) {
+                return product.length;
+            }
+            return '';
+        };
+
+        $scope.shelfProductDisplayLength = function(product) {
+            if ($scope.displayShortNameExtensionForProduct.indexOf(product.type.name) !== -1) {
+                return product.short_name_extension;
+            }
+            return '';
+        };
+
         $scope.getStore = function () {
-
-            // Product.getByType().then(function (response) {
-            //     $scope.heads = response.data['heads'];
-            //     $scope.poles = response.data['poles'];
-            //     $scope.shrinker = response.data['shrinker'];
-            //     $scope.extras1 = response.data['extras'].slice(0, 3);
-            //     $scope.extras2 = response.data['extras'].slice(3, 7);
-            //     $scope.extras3 = response.data['extras'].slice(7, 11);
-            //     $scope.apparel = response.data['apparel'];
-            //     $scope.glasses = response.data['glasses'];
-            //     $scope.packages = response.data['packages'];
-            // }, function (error) {
-            //     $exceptionHandler(JSON.stringify(error));
-            //     console.log("Something went wrong getting the store data.");
-            // });
-
             Product.allAvailabel().then(function(response) {
                 $scope.products = response.data;
             }, function (error) {
--- resources/views/partials/store.blade.php
+++ resources/views/partials/store.blade.php
@@ -1,246 +1,38 @@
 <?php
-//	$headsRow = '
-//	<div class="heads-row product-row">
-//		<div 	class="product-thumb-container"
-//				ng-repeat="head in heads">
-//				<a href="' . route('product.show', ['']) . '/{{ head.slug }}">
-//					<img ng-src="{{ head.thumb }}">
-//				</a>
-//		</div>
-//
-//		<div class="clear"></div>
-//
-//	</div>
-//
-//	<div class="heads-links links">
-//		<div 	class="product-link-container"
-//				ng-repeat="head in heads">
-//				<a href="' . route('product.show', ['']) . '/{{ head.slug }}">{{ head.shortName }}
-//					<span class="detail" ng-if="head.slug === \'signature-paralyzer\' || head.slug === \'barbed-paralyzer\'"> /{{ head.length }}</span>
-//				</a>
-//		</div>
-//
-//		<div class="clear"></div>
-//	</div>
-//
-//	<h2 class="store-label">Detachable Heads</h2>';
-//
-//	$tailsRow = '
-//	<div class="tails-row product-row">
-//		<div 	class="product-thumb-container"
-//				ng-repeat="pole in poles">
-//				<a href="' . route('product.show', ['']) . '/{{ pole.slug }}">
-//					<img ng-src="{{ pole.thumb }}">
-//				</a>
-//		</div>
-//		<div class="clear"></div>
-//	</div>
-//
-//	<div class="tails-links links">
-//
-//		<div 	class="product-link-container"
-//				ng-repeat="pole in poles"
-//				ng-cloak>
-//				<a href="' . route('product.show', ['']) . '/{{ pole.slug }}">
-//					<!-- Don\'t breake following line in separate lines to avoid spaces between words. -->
-//					<span class="default-product-name">{{ pole.shortName }}<span class="unbold">{{ pole.short_name_extension }}</span><span class="detail uncaps">/{{ pole.length }}</span>
-//					</span>
-//					<span class="mobile-product-name">{{ (pole.mobile_name) ? pole.mobile_name : "" }}</span>
-//				</a>
-//		</div>
-//
-//		<div class="clear"></div>
-//
-//	</div>
-//
-//	<h2 class="store-label">Gen 2 Hybrid Polespears</h2>';
-//
-//	$packageRow = '
-//	<div class="extras-row product-row">
-//		<div 	class="product-thumb-container"
-//				ng-repeat="package in packages">
-//				<pre>{{package.type}}</pre>
-//					<a href="' . route('product.show', ['']) . '/{{ package.slug }}">
-//						<img ng-src="{{ package.thumb }}"
-//							 ng-class="{\'bands-thumb-large\' : package.slug == \'Blue-Water\' || package.slug == \'lionfish\' || package.slug == \'dinner-getter\' ||  package.slug == \'Bahamas-Basics\' }">
-//					</a>
-//		</div>
-//		<div class="clear"></div>
-//	</div>
-//
-//	<div class="links glass-links">
-//		<div class="product-link-container" ng-repeat="package in packages">
-//			<a href="' . route('product.show', ['']) . '/{{ package.slug }}">{{ package.shortName }}</a>
-//		</div>
-//		<div class="clear"></div>
-//	</div>
-//		<h2 class="store-label">Packages</h2>';
-//
-//	$extras1Row = '
-//	<div class="extras-row product-row">
-//
-//		<div 	class="product-thumb-container"
-//				ng-repeat="shrinkerItem in shrinker">
-//				<a href="' . route('product.show', ['']) . '/{{ shrinkerItem.slug }}">
-//					<img ng-src="{{ shrinkerItem.thumb }}">
-//				</a>
-//		</div>
-//
-//		<div 	class="product-thumb-container"
-//				ng-repeat="extra in extras1"
-//				ng-show="extra.availabilityTypeId == 1">
-//				<a href="' . route('product.show', ['']) . '/{{ extra.slug }}">
-//					<img 	ng-src="{{ extra.thumb }}"
-//							ng-class="{ \'knife-thumb\' : extra.slug == \'budk\', \'bands-thumb\' : extra.slug == \'bands\' || extra.slug == \'hardcore-bands\'}">
-//				</a>
-//		</div>
-//
-//		<div class="clear"></div>
-//
-//	</div>
-//
-//	<div class="extras-links links">
-//
-//		<div 	class="product-link-container"
-//				ng-repeat="shrinkerItem in shrinker">
-//				<a href="' . route('product.show', ['']) . '/{{ shrinkerItem.slug }}">{{ shrinkerItem.name }}</a>
-//		</div>
-//
-//		<div 	class="product-link-container"
-//				ng-repeat="extra in extras1"
-//				ng-show="extra.availabilityTypeId == 1">
-//				<a href="' . route('product.show', ['']) . '/{{ extra.slug }}">{{ extra.name }}</a>
-//		</div>
-//
-//		<div class="clear"></div>
-//
-//	</div>
-//
-//	<h2 class="store-label">Accessories</h2>';
-//
-//	$extras2Row = '
-//	<div class="extras-row product-row">
-//
-//		<div 	class="product-thumb-container"
-//				ng-repeat="extra in extras2"
-//				ng-show="extra.availabilityTypeId == 1">
-//				<a href="' . route('product.show', ['']) . '/{{ extra.slug }}">
-//					<img ng-src="{{ extra.thumb }}" ng-class="{ \'knife-thumb\' : extra.slug == \'budk\', \'bands-thumb\' : extra.slug == \'bands\', \'bands-thumb-large\' : extra.slug == \'hardcore-bands\', \'monty-thumb\' : extra.slug == \'full-monty\', \'big-game-thumb\' : extra.slug == \'big-game\', \'cable-w-tip-thumb\' : extra.slug == \'cable-w-tip\' }">
-//				</a>
-//		</div>
-//
-//		<div class="clear"></div>
-//
-//	</div>
-//
-//	<div class="extras-links links">
-//
-//		<div 	class="product-link-container"
-//				ng-repeat="extra in extras2"
-//				ng-show="extra.availabilityTypeId == 1">
-//				<a href="' . route('product.show', ['']) . '/{{ extra.slug }}">{{ extra.name }}</a>
-//		</div>
-//
-//		<div class="clear"></div>
-//
-//	</div>
-//
-//	<h2 class="store-label">Accessories</h2>';
-//
-//	$extras3Row = '
-//	<div class="extras-row product-row">
-//		<div 	class="product-thumb-container"
-//				ng-repeat="extra in extras3"
-//				ng-show="extra.availabilityTypeId == 1">
-//				<a href="' . route('product.show', ['']) . '/{{ extra.slug }}">
-//					<img 	ng-src="{{ extra.thumb }}"
-//							ng-class="{ \'knife-thumb\' : extra.slug == \'budk\', \'bands-thumb\' : extra.slug == \'bands\' || extra.slug == \'hardcore-bands\' || extra.slug == \'offshore-striker\', \'monty-thumb\' : extra.slug == \'full-monty\', \'big-game-thumb\' : extra.slug == \'big-game\', \'cable-w-tip-thumb\' : extra.slug == \'cable-w-tip\' || extra.slug == \'cable\' || extra.slug == \'tips\', \'flashers-thumb\' : extra.slug == \'fin-flashers\' }">
-//				</a>
-//		</div>
-//
-//		<div class="clear"></div>
-//	</div>
-//
-//	<div class="extras-links links">
-//
-//		<div 	class="product-link-container"
-//				ng-repeat="extra in extras3"
-//				ng-show="extra.availabilityTypeId == 1">
-//				<a href="' . route('product.show', ['']) . '/{{ extra.slug }}">{{ extra.name }}</a>
-//		</div>
-//
-//		<div class="clear"></div>
-//	</div>
-//
-//	<h2 class="store-label">Accessories</h2>';
-//
-//	$apparelRow = '
-//	<div class="apparel-row product-row">
-//
-//		<div 	class="product-thumb-container"
-//				ng-repeat="apparelItem in apparel">
-//				<a href="' . route('product.show', ['']) . '/{{ apparelItem.slug }}">
-//					<img ng-src="{{ apparelItem.thumb }}">
-//				</a>
-//		</div>
-//
-//		<div class="clear"></div>
-//
-//	</div>
-//
-//	<div class="apparel-links links">
-//
-//		<div class="product-link-container" ng-repeat="apparelItem in apparel"><a href="' . route('product.show', ['']) . '/{{ apparelItem.slug }}">{{ apparelItem.name }}</a></div>
-//
-//		<div class="clear"></div>
-//
-//	</div>
-//
-//	<h2 class="store-label">Apparel</h2>';
-//
-//	$glassRow = '
-//	<div class="product-row glass-row">
-//		<div 	class="product-thumb-container"
-//				ng-repeat="glass in glasses">
-//				<a href="' . route('product.show', ['']) . '/{{ glass.slug }}">
-//					<img ng-src="{{ glass.thumb }}">
-//				</a>
-//		</div>
-//	</div>
-//
-//	<div class="links glass-links">
-//		<div 	class="product-link-container"
-//				ng-repeat="glass in glasses">
-//				<a href="' . route('product.show', ['']) . '/{{ glass.slug }}">{{ glass.shortName }}
-//					<span class="detail"> /{{ glass.length }}</span>
-//				</a>
-//		</div>
-//
-//		<div class="clear"></div>
-//	</div>
-//
-//	<h2 class="store-label">Glasses</h2>';
-
 	$shelves = '
 		<div ng-repeat="shelf in shelves">
 
-			<div class="heads-row product-row">
-				<div 	class="product-thumb-container"
+			<div class="product-on-shelf-row product-row">
+				<div 	class="{{getThumbClass(product)}}"
 						ng-repeat="product in products"
-						ng-show="product.shelf_id == shelf.id">
+						ng-if="product.shelf_id == shelf.id">
 					<a href="' . route('product.show', ['']) . '/{{ product.slug }}">
-						<img ng-src="{{ product.thumb }}">
+						<img 	ng-src="{{ product.thumb }}"
+								ng-class="{ \'knife-thumb\' : product.slug == \'budk\',
+											\'bands-thumb\' : product.slug == \'bands\' || product.slug == \'hardcore-bands\' || product.slug == \'offshore-striker\',
+											\'monty-thumb\' : product.slug == \'full-monty\',
+											\'big-game-thumb\' : product.slug == \'big-game\',
+											\'cable-w-tip-thumb\' : product.slug == \'cable-w-tip\' || product.slug == \'cable\' || product.slug == \'tips\',
+											\'flashers-thumb\' : product.slug == \'fin-flashers\',
+											\'bands-thumb-large\' : product.slug == \'Blue-Water\' || product.slug == \'lionfish\' || product.slug == \'dinner-getter\' ||  product.slug == \'Bahamas-Basics\'
+											}">
 					</a>
 				</div>
 				<div class="clear"></div>
 			</div>
 
-			<div class="heads-links links">
-				<div 	class="product-link-container"
+			<div class="product-on-shelf-links links">
+				<div 	class="{{getLinkClass(product)}}"
 						ng-repeat="product in products"
-						ng-show="product.shelf_id == shelf.id">
-					<a href="' . route('product.show', ['']) . '/{{ product.slug }}">{{ product.shortName }}
-						<span class="detail" ng-if="product.slug === \'signature-paralyzer\' || product.slug === \'barbed-paralyzer\'"> /{{ product.length }}</span>
+						ng-if="product.shelf_id == shelf.id">
+					<a href="' . route('product.show', ['']) . '/{{ product.slug }}">
+						{{shelfProductNameToDisplay(product)}}{{$scope.shelfProductDisplayLength(product)}}{{shelfProductDisplayLength(product)}}
+
+						<span class="detail"
+							ng-if="product.slug === \'signature-paralyzer\' || product.slug === \'barbed-paralyzer\'"> /{{ product.length }}
+						</span>
+
+						<span class="mobile-product-name">{{ (product.mobile_name) ? product.mobile_name : "" }}</span>
 					</a>
 				</div>
 
@@ -253,53 +45,6 @@
 	';
  ?>
 
-
 <div class="store-container" ng-controller="StoreController">
-
-	{{--store.blade.php--}}
 	{!! $shelves !!}
-
-
-	{{--<div ng-repeat="shelf in shelves">--}}
-		{{--<span>{{ shelf.name }}</span>--}}
-	{{--</div>--}}
-
-	{{--@if(isset($product))--}}
-
-		{{--@if($product->type->slug === 'head')--}}
-
-		{{--{!! $headsRow . $tailsRow . $packageRow . $extras1Row . $extras2Row . $extras3Row . $apparelRow . $glassRow !!}--}}
-
-		{{--@elseif($product->type->slug === 'pole')--}}
-
-		{{--{!! $tailsRow . $packageRow . $headsRow . $extras1Row . $extras2Row . $extras3Row . $apparelRow . $glassRow !!}--}}
-
-		{{--@elseif($product->type->slug === 'extra' || $product->type->slug === 'shrinker')--}}
-
-		{{--{!! $extras1Row . $extras2Row . $extras3Row . $headsRow . $tailsRow . $packageRow . $apparelRow . $glassRow !!}--}}
-
-		{{--@elseif($product->type->slug === 'apparel')--}}
-
-		{{--{!! $apparelRow . $headsRow . $tailsRow . $packageRow . $extras1Row . $extras2Row . $extras3Row . $glassRow !!}--}}
-
-		{{--@elseif($product->type->slug === 'glass')--}}
-
-		{{--{!! $glassRow . $headsRow . $tailsRow . $packageRow . $extras1Row . $extras2Row . $extras3Row . $apparelRow !!}--}}
-
-		{{--@elseif($product->type->slug === 'package')--}}
-
-		{{--{!! $packageRow. $headsRow . $tailsRow . $extras1Row . $extras2Row . $extras3Row . $apparelRow . $glassRow !!}--}}
-
-		{{--@else--}}
-
-		{{--{!! $headsRow . $tailsRow . $packageRow . $extras1Row . $extras2Row . $extras3Row . $apparelRow . $glassRow !!}--}}
-
-		{{--@endif--}}
-
-	{{--@else--}}
-
-	{{--{!! $headsRow . $tailsRow . $packageRow . $extras1Row . $extras2Row . $extras3Row . $apparelRow . $glassRow !!}--}}
-
-	{{--@endif--}}
-
 </div>
