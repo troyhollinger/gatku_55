b04a3465e47e0c4c909a7f1dd864507aee6b61fb
Marcin Wojcik <marcincyniu@gmail.com>
2018-11-07 18:38:51 -0800

    feature/200 Make Bugsnag report only for QA and production. On dev error should be reported on console.

--- public/js/config.js
+++ public/js/config.js
@@ -4,19 +4,22 @@
 //This function allow to avoid exceptions in some cases
 function checkIfExceptionShouldBeSent(exception) {
 	var notyfy = true;
+
+	//Check if one of below options to avoid send exception to Bugsnag
 	switch(exception) {
 		case 'Possibly unhandled rejection: close':
 			notyfy = false;
 			break;
 	}
+
 	return notyfy;
 }
 
+//Bugsnag javascript setup
 angular
     .module('exceptionOverride', [])
     .factory('$exceptionHandler', function () {
         return function (exception, cause) {
-
         	if (checkIfExceptionShouldBeSent(exception)) {
                 bugsnagClient.notify(exception, {
                     beforeSend: function (report) {
@@ -24,9 +27,13 @@ angular
                     }
                 })
 			}
+
+			//This is code responsible for reporting error in console when dev environment
+            if (['production', 'qa', 'QA'].indexOf(CONFIG.environment) === -1) {
+                console.error(exception);
+            }
         }
     });
-
 //Bugsnag configuration end.
 
 var app = angular.module('gatku', [
@@ -50,6 +57,7 @@ app.config(function(stripeProvider, $locationProvider) {
 	//This hack works for: $routeProvider
     $locationProvider.hashPrefix('');
 
+    //@TODO this keys should not be hardcoded here. This is bad practice. Refactor code to use .env variable.
 	if (CONFIG.environment === 'production') {
 		stripeProvider.setPublishableKey('pk_live_5MrQVqT1OSrL1lyeYe54NWgs');
 	} else {
--- public/production/app.js
+++ public/production/app.js
@@ -12072,19 +12072,22 @@ for (key in angularFileUpload) {
 //This function allow to avoid exceptions in some cases
 function checkIfExceptionShouldBeSent(exception) {
 	var notyfy = true;
+
+	//Check if one of below options to avoid send exception to Bugsnag
 	switch(exception) {
 		case 'Possibly unhandled rejection: close':
 			notyfy = false;
 			break;
 	}
+
 	return notyfy;
 }
 
+//Bugsnag javascript setup
 angular
     .module('exceptionOverride', [])
     .factory('$exceptionHandler', function () {
         return function (exception, cause) {
-
         	if (checkIfExceptionShouldBeSent(exception)) {
                 bugsnagClient.notify(exception, {
                     beforeSend: function (report) {
@@ -12092,9 +12095,13 @@ angular
                     }
                 })
 			}
+
+			//This is code responsible for reporting error in console when dev environment
+            if (['production', 'qa', 'QA'].indexOf(CONFIG.environment) === -1) {
+                console.error(exception);
+            }
         }
     });
-
 //Bugsnag configuration end.
 
 var app = angular.module('gatku', [
@@ -12118,6 +12125,7 @@ app.config(function(stripeProvider, $locationProvider) {
 	//This hack works for: $routeProvider
     $locationProvider.hashPrefix('');
 
+    //@TODO this keys should not be hardcoded here. This is bad practice. Refactor code to use .env variable.
 	if (CONFIG.environment === 'production') {
 		stripeProvider.setPublishableKey('pk_live_5MrQVqT1OSrL1lyeYe54NWgs');
 	} else {
