a3a177d93a169f2579bc38e1b678562ec1ef6a0a
Marcin Wojcik <marcincyniu@gmail.com>
2019-02-27 18:23:22 -0800

    feature/96 Add CalculateOrderService. WIP.

--- app/Gatku/Services/CalculateOrdersService.php
+++ app/Gatku/Services/CalculateOrdersService.php
@@ -1,6 +1,8 @@
 <?php 
 namespace Gatku\Service;
 
+use Gatku\Model\Discount;
+use Gatku\Model\HomeSetting;
 use Gatku\Model\Order;
 
 class CalculateOrdersService
@@ -12,7 +14,7 @@ class CalculateOrdersService
     /**
      * @var int
      */
-    private $discount;
+    private $discountAmount;
     /**
      * @var int
      */
@@ -25,40 +27,217 @@ class CalculateOrdersService
      * @var int
      */
     private $total;
+    /**
+     * @var Order
+     */
+    private $order;
+    /**
+     * @var Discount
+     */
+    private $discount;
+    /**
+     * @var HomeSetting
+     */
+    private $homeSetting;
+
+    private $freeShippingAmountTheshold = 30000; //This is $300
+    /**
+     * CalculateOrdersService constructor.
+     * @param HomeSetting $homeSetting
+     */
+    public function __construct(HomeSetting $homeSetting)
+    {
+        $this->homeSetting = $homeSetting;
+    }
 
     /**
      * @param Order $order
+     * @param Discount $discount
+     * @return array
      */
-    public function calculateSubtotal(Order $order) {
+    public function getOrderCalculations(Order $order, ?Discount $discount) {
 
-    }
+        $this->order = $order;
+        $this->discount = $discount;
 
-    public function calculateDiscount() {
-        return 'calculateDiscount';
-    }
-
-    public function calculateShipping() {
-
-    }
-
-    public function calculateTax() {
-
-    }
-
-    public function calculateTotal() {
-
-    }
-
-    public function getAllCalculatedValues(Order $order) {
-        $this->calculateSubtotal($order);
+        $this->subtotal = $this->calculateSubtotal();
+        $this->discountAmount = $this->calculateDiscountAmount();
+        $this->shipping = $this->calculateShipping();
+        $this->tax = $this->calculateTax();
+        $this->total = $this->calculateTotal();
 
         return [
             'subtotal' => $this->subtotal,
-            'discount' => $this->discount,
+            'discount' => $this->discountAmount,
             'shipping' => $this->shipping,
             'tax' => $this->tax,
             'total' => $this->total
         ];
+    }
+
+
+    /**
+     * @return float|int
+     */
+    private function calculateSubtotal() {
+        $subtotal = 0;
+
+        $items = $this->order->items;
+
+        //$discountReverse - variable name because this calculate actually not a discount but how much should be discounted price?
+        $discountReverse = 1;
+        if ($this->discount) {
+            $discountReverse = (100 - $this->discount->discount) / 100;
+        }
+
+        foreach($items as $item) {
+
+            if ($item->product->sizeable && $item->sizeId) {
+                $price = $item->size->price;
+            } else {
+                //This id done because we don't wand to double price for charging
+                //Then package price is not added. Only elements from package are summed.
+                if ($item->product->type->slug != 'package') {
+                    $price = $item->product->price;
+                } else {
+                    $price = 0;
+                }
+            }
+
+            //Calc item value
+//@TODO should we remove following line?
+            $value = $this->calculateDiscountValue($price, $item->quantity, $discountReverse);
+            $subtotal += $value;
+
+            foreach($item->addons as $addon) {
+
+                //For Addons with price_zero = 1
+                if ($addon->price_zero) {
+                    $addonPrice = 0;
+                } else {
+                    if ($addon->product->sizeable && $addon->sizeId) {
+                        $addonPrice = $addon->size->price;
+                    } else {
+                        $addonPrice = $addon->product->price;
+                    }
+                }
+
+                //Calc addon value
+//@TODO Should we remove following line
+                $addonPrice = $this->calculateDiscountValue($addonPrice, $addon->quantity, $discountReverse);
+                $subtotal += $addonPrice;
+            }
+        }
+        //This is hardcoded discount for Black Friday, consider remove this code
+//@todo should we calc here ?? I don't think so
+//$discountHardcoded = $this->calculateDiscount();
+
+        return $subtotal;
+    }
+
+    /**
+     * @return float|int
+     */
+    private function calculateDiscountAmount() {
+        $amount = 0;
+        $glassCheck = 0;
+        $glassPrice = 0;
+
+        if ($this->subtotal && $this->homeSetting->global_discount_switch) {
+            $amount = intval($this->subtotal * ( $this->homeSetting->global_discount_percentage / 100 ));
+            return $amount;
+        }
+
+        $items = $this->order->items;
+
+        //Glass discount
+        foreach($items as $item) {
+            if ($item->product->type->slug === 'glass') {
+                $glassCheck += $item->quantity;
+                $glassPrice = $item->product->price;
+            }
+
+            foreach($item->addons as $addon) {
+                if ($addon->product->type->slug === 'glass') {
+                    $glassCheck += $addon->quantity;
+                }
+            }
+        }
+
+        if ($glassCheck >= 4) {
+            $amount = ($glassPrice * 4) - 4000;
+        }
+
+        return intval($amount);
+    }
+
+    /**
+     * @return float|int
+     */
+    private function calculateShipping() {
+        $shippingPrice = 0;
+        $poles = [];
+        $heads = [];
+        $others = [];
+
+        $items = $this->order->items;
+
+        if ($this->subtotal >= $this->freeShippingAmountTheshold) return 0;
+
+        foreach($items as $item) {
+            if ($item->product->type->slug === 'pole') {
+                $poles[] = $item;
+            } elseif ($item->product->type->slug === 'head') {
+                $heads[] = $item;
+            } else {
+                $others[] = $item;
+            }
+        }
+
+        if (count($poles) > 0) {
+            $poleShippingPrice = $poles[0]->product->type->shippingPrice;
+
+            if (count($poles) > 1) {
+                $shippingPrice = $poleShippingPrice * count($poles);
+            } else {
+                $shippingPrice = $poleShippingPrice;
+            }
+        } elseif (count($heads) > 0) {
+            $headShippingPrice = $heads[0]->product->type->shippingPrice;
+            if (count($heads) > 1) {
+                $shippingPrice = $headShippingPrice * ceil(count($heads) / 2);
+            } else {
+                $shippingPrice = $headShippingPrice;
+            }
+        } elseif (count($others) > 0) {
+            $shippingPrice = $others[0]->product->type->shippingPrice;
+        }
+
+        return $shippingPrice;
+    }
+
+    /**
+     * @return int
+     */
+    private function calculateTax() {
+        $taxAmount = intval( ( $this->subtotal + $this->shipping ) * ( $this->order->sales_tax / 100) );
+        return $taxAmount;
+    }
+
+    private function calculateTotal() {
+        $total = $this->subtotal + $this->shipping + $this->tax - $this->discountAmount;
+        return $total;
+    }
+
+    /**
+     * @param $price
+     * @param $quantity
+     * @param $discountReverse
+     * @return float|int
+     */
+    private function calculateDiscountValue($price, $quantity, $discountReverse)
+    {
+        return ($price * $quantity) * $discountReverse;
     }
 }
 
--- app/tests/Gatku/Services/CalculateOrdersServiceTest.php
+++ app/tests/Gatku/Services/CalculateOrdersServiceTest.php
@@ -1,33 +1,66 @@
 <?php
 
+use Gatku\Model\Discount;
+use Gatku\Model\Order;
+use Gatku\Repositories\OrderRepository;
 use Tests\TestCase;
 use Gatku\Service\CalculateOrdersService;
-
 
 class CalculateOrdersServiceTest extends TestCase {
 
     /**
      * @var CalculateOrdersService
      */
-    public $service;
+    private $calculateOrdersService;
+    /**
+     * @var OrderRepository
+     */
+    private $orderRepository;
+    /**
+     * @var Order
+     */
+    private $order;
+    /**
+     * @var Discount
+     */
+    private $discount;
 
     public function setUp()
     {
         parent::setUp();
 
-        $this->service = $this->app->make(CalculateOrdersService::class);
+        //Set variables
+        $this->calculateOrdersService = $this->app->make(CalculateOrdersService::class);
+        $this->orderRepository = $this->app->make(OrderRepository::class);
+
+        $this->getOrder();
+        $this->setNoDiscount();
     }
 
-	public function testTrue()
+    public function testCalculateSubtotal()
     {
-		$this->assertTrue(true);
-	}
+        $result = $this->calculateOrdersService->getOrderCalculations($this->order, $this->discount);
 
-	public function testCalculateDiscount()
-    {
-        $result = $this->service->calculateDiscount();
+        $this->assertInternalType('array', $result);
 
-        $this->assertEquals('calculateDiscount', $result);
+        $expectedValue = 4900;
+        $this->assertEquals($expectedValue, $result['subtotal']);
     }
 
+    private function getOrder()
+    {
+        $this->order = $this->orderRepository->get(2136);
+    }
+
+    private function setNoDiscount()
+    {
+        $this->discount = null;
+    }
+
+    private function setDiscount()
+    {
+        $this->discount = new Discount;
+        $this->discount->code = 'test_code_10';
+        $this->discount->discount = 10;
+    }
 }
