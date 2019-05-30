fd379a0240d5f4b8600fd6543820416a648d2a2f
Marcin Wojcik <marcincyniu@gmail.com>
2019-05-29 18:59:55 -0700

    feature/233 Update queries to populate product tables.

--- database/patches/2019-05-29_crists_populate_shipping_html.sql
+++ database/patches/2019-05-29_crists_populate_shipping_html.sql
@@ -1,5 +1,6 @@
-/* Populate free_shipping_html column for gatku.com */
+/* Populate free_shipping_html column for: cristpears.com */
 
 -- table: email_settings:
 UPDATE products
-SET free_shipping_html = '<span class="bold">Free Shipping</span> on orders over <span class="bold">$300</span> <span class="detail bold">USA ONLY</span>';
+SET free_shipping_html = '<span class="bold">Free Shipping</span> on orders over <span class="bold">$300</span> <span class="detail bold">USA ONLY</span>'
+WHERE id <> 1000;
