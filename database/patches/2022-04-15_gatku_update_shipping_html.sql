390613bd8e1105ee23f4496e931e39b72c53dd53
Marcin Wojcik <marcincyniu@gmail.com>
2022-04-15 13:59:33 -0700

    MISC Update query.

--- database/patches/2022-04-15_gatku_update_shipping_html.sql
+++ database/patches/2022-04-15_gatku_update_shipping_html.sql
@@ -3,4 +3,4 @@
 -- table: email_settings:
 UPDATE products
 SET free_shipping_html = '<span class="bold">Free Shipping</span> on orders over <span class="bold">$400</span> <span class="detail bold">USA + AU ONLY</span>'
-WHERE id <> 1000;
+WHERE id <> 1000 AND free_shipping_html IS NOT NULL;
