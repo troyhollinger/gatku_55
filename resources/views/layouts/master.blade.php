df915b2e11628939ab4f0b18a459f523b198a1d5
Marcin Wojcik <marcincyniu@gmail.com>
2024-02-25 11:39:19 -0800

    Add Google Tag Magager code to page.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -152,6 +152,14 @@
                     gtag('config', 'AW-879212783');
                 </script>
             <!-- Global site tag (gtag.js) - Google Ads: 879212783 - End -->
+
+            <!-- Google Tag Manager -->
+                <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-WZ29ZPSS');</script>
+            <!-- End Google Tag Manager -->
         @endif
 
     </head>
@@ -184,6 +192,11 @@
             });
         </script>
         <!-- FRESHCHAT WIDGET - loading - start -->
+
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WZ29ZPSS"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
     @endif
 
     </body>
